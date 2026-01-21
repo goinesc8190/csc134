91d622b999a70d4cad469d602f7701d73f2b360a
goinesc8190 <goinesc8190@student.faytechcc.edu>
2026-01-21 16:32:11 +0000

    Finished M1 lab

--- m1lab_goines.cpp
+++ m1lab_goines.cpp
@@ -1,12 +1,27 @@
 // CSC 134   
-// M1LAB1
+// M1LAB1 - We're selling something
 // Goines     
 // 01/21/2026
 
 #include <iostream>
+#include <iomanip> // for decimal places 
 using namespace std;
 
 int main() {
-    return 0;
+    // Declare my variables 
+    string item_name = "tools";
+    int item_count = 100; 
+    double item_cost = 5.00; 
+    double total_cost; // don't know yet
+    
+    // Do the work - Welcome them to the store.
+    cout << "Welcome to the "<< item_name << " store. " << endl;
+    cout << " We have "  <<  item_count << " " << item_name <<  "."  << endl;
+    cout << " They cost $ " << item_cost << " each. " << endl; 
+
+    // Calculate the total price
+    total_cost = item_count * item_cost;
+    // Print the total
+    cout << "To buy all of them will cost $"<< total_cost << endl;
 }
     
