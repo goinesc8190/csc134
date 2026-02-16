78cc6100e2a938d5d6f5d9fec4e31544f82ac76e
goinesc8190 <goinesc8190@student.faytechcc.edu>
2026-02-16 16:33:18 +0000

    Checking in

--- M3/m3t2.cpp
+++ M3/m3t2.cpp
@@ -5,6 +5,8 @@
 // Start the game of craps.
 #include <iostream>
 using namespace std;
+#include <cstdlib> // for random
+#include <ctime> //for time
 
 // Function declarations
 // (Tell the program what functions will be!)
@@ -12,13 +14,25 @@ int roll();
 
 //main() goes here
 int main() {
+   // Seed the random number generator
+   int seed = time (0);
+   srand(seed);
+   
     int number = roll ();
-    cout << "You rolled a" << number << endl;
+    int first, second, total;
+    first = roll ();
+    second = roll ();
+    total = first + second;
+    cout << "You rolled a " << first << "+" << second << "=" << total << endl;
     return 0;
 }
 
 // Function definitions
 // (Actually write the Entire function!)
 int roll() {
-    return 1; // these dice are loaded!
+    // rand() gives a large random number
+    // % 6 divides by six, anf keeps the remainder
+    // finally, add 1 so it'o to five.
+    int my_roll = (rand() % 6) + 1;
+    return my_roll; 
 }
