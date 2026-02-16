31bdf501a2ff3ebc4ad32e8a86f0af0c2cbb0230
goinesc8190 <goinesc8190@student.faytechcc.edu>
2026-02-16 22:06:25 +0000

    complete

---
+++ M2/m2hw1q2_goines.cpp
@@ -0,0 +1,57 @@
+/*
+CSC 134
+M2hw1q2 - The Crate Program
+goines
+2/16/26
+
+Find volume of crate
+Find cost, customer_price, and profit
+Print everything out
+*/
+
+# include <iostream>
+# include <iomanip>
+using namespace std;
+
+
+
+int main() {
+    // Part 1--Declare Variables
+    // constant variables (don't change unless the market changes)
+    const double COST_PER_CUBIC_FOOT = 0.30;
+    const double CHARGE_PER_CUBIC_FOOT = 0.52;
+    
+    // Crate variables
+    double length, width, height, volume; //all in feet
+    // Money variables
+    double cost, customer_price, profit; // all in $
+
+    // Part 2 -- Get the Input
+    cout << "Welcome to the Crate Program" << endl;
+    cout << "Enter the crate dimensions." << endl;
+
+    cout << "Crate Length? ";
+    cin >> length;
+    cout << "Crate Width? ";
+    cin >> width;
+    cout << "Crate Height? ";
+    cin >> height;
+
+    // Part 3 -- Do the Calculations
+    volume = length * width * height;
+    // Find the cost and the customer_price
+    cost = volume * COST_PER_CUBIC_FOOT;
+    customer_price = volume * CHARGE_PER_CUBIC_FOOT;
+    profit = customer_price - cost; 
+
+    // Part 4 -- Print the output
+    cout << setprecision(2) << fixed; // 2 decimal spaces
+    cout << " ==== CRATE INFO ==== " << endl;
+    cout << "Volume is " << volume <<  " cubic feet." << endl;
+    cout << "Wholesale price is $ " <<  cost << endl;
+    cout << "Customer price is  $ " << customer_price << endl;
+    cout << "Profit per crate   $ " << profit << endl;
+
+    return 0;
+
+}
