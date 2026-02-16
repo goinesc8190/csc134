5070adeb1552f748fb67af70e11572a0cff4d723
goinesc8190 <goinesc8190@student.faytechcc.edu>
2026-02-16 15:50:01 +0000

    added roll function

--- M2/m2hw1_goines.cpp
+++ M2/m2hw1_goines.cpp
@@ -1,7 +1,41 @@
 /*
 CSC 134
-M2H1 - Gold
+M2HW1 - Gold
 Chris Goines
 2/15/2026
 */
 
+#include <iostream>
+#include <iomanip> // for decimal places 
+using namespace std;
+
+int main() { 
+// Declare variables
+string name;
+int accountbalance; 
+double deposit;
+double withdrawal;
+double finalbalance;
+double witfinalbalance;
+double depfinalbalance;
+
+cout << "What is your name?" <<endl;
+    cin >> name; 
+    cout << "What is your starting account balance?" <<endl;
+    cin >> accountbalance;
+    cout << "How much is your deposit?" <<endl;
+    cin >> deposit; 
+    cout << "What is the amount of your withdrawal?" <<endl;
+    cin >> withdrawal;
+    
+    // Do the work.
+    cout << setprecision(2) <<fixed;
+    cout << name << endl;
+    cout << "Account Number: 52378429"<< endl;
+    cout << "Withdrawal Final Balance"<< ; witfinalbalance; endl;
+    cout << "Deposit Final Balance"; depfinalbalance; 
+    
+    // Calculate finalbalance;
+     witfinalbalance = accountbalance - withdrawal; 
+     depfinalbalance = accountbalance + deposit; 
+}
---
+++ M3/m3t2.cpp
@@ -0,0 +1,24 @@
+// CSC 134
+// M3T2 - Craps!
+// goinesc
+// 2/16/26
+// Start the game of craps.
+#include <iostream>
+using namespace std;
+
+// Function declarations
+// (Tell the program what functions will be!)
+int roll(); 
+
+//main() goes here
+int main() {
+    int number = roll ();
+    cout << "You rolled a" << number << endl;
+    return 0;
+}
+
+// Function definitions
+// (Actually write the Entire function!)
+int roll() {
+    return 1; // these dice are loaded!
+}
