6858144dc506129582c7fe08bf047bd0a28b0d82
goinesc8190 <goinesc8190@student.faytechcc.edu>
2026-02-17 03:56:16 +0000

    need calculations

---
+++ M2/m2hw1q3_goines.cpp
@@ -0,0 +1,33 @@
+/*  
+CSC 134
+M2hw1q3 - Pizza Party
+goines
+2/16/26
+*/
+
+#include <iostream>
+#include <iomanip> 
+using namespace std;
+
+int main() { 
+    string name;
+    int numpizzas; // nine slices per pizza
+    double slicepizza; // each visitor 3 slices
+    double total_slices;
+    double numvisitors;
+    double leftoverpizza;
+
+    cout << "How many pizzas would you like?" << endl;
+         cin >> numpizzas;
+         cout <<"How many slices per pizza?" << endl;
+         cin >> slicepizza;
+         cout <<"How many visitors are coming?" << endl;
+         cin >> numvisitors;
+ 
+  
+    
+  // Dothe work
+   cout << "Pieces of pizza left over" << leftoverpizza << endl;
+     
+   
+}
