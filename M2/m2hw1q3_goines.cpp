84004b8ab24c9b0ad64d9cbe6ba7c3c1b9e3a86b
goinesc8190 <goinesc8190@student.faytechcc.edu>
2026-02-18 00:43:21 +0000

    Finished w/decimal bug/inst answer

--- M2/m2hw1q3_goines.cpp
+++ M2/m2hw1q3_goines.cpp
@@ -12,22 +12,24 @@ using namespace std;
 int main() { 
     string name;
     int numpizzas; // nine slices per pizza
-    double slicepizza; // each visitor 3 slices
-    double total_slices;
+    double slicespizza;
+    double slicesperguest = 3.0;
     double numvisitors;
     double leftoverpizza;
 
     cout << "How many pizzas would you like?" << endl;
          cin >> numpizzas;
          cout <<"How many slices per pizza?" << endl;
-         cin >> slicepizza;
+         cin >> slicespizza;
          cout <<"How many visitors are coming?" << endl;
          cin >> numvisitors;
  
+    // Caculate final
+    leftoverpizza = numpizzas * slicespizza - numvisitors * slicesperguest;
   
-    
-  // Dothe work
-   cout << "Pieces of pizza left over" << leftoverpizza << endl;
+    // Do the work
+    cout << setprecision(2) <<fixed;
+    cout << "Pieces of pizza left over = " << leftoverpizza << endl;
      
-   
+    
 }
