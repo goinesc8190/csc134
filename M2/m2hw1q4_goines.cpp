885f501d787394eee1c8cd7ef06e1a40cf51e8dc
goinesc8190 <goinesc8190@student.faytechcc.edu>
2026-02-18 02:12:31 +0000

    incomplete

---
+++ M2/m2hw1q4_goines.cpp
@@ -0,0 +1,25 @@
+/*  
+CSC 134
+M2hw1q4 - Cheering Program
+goines
+2/17/26
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() { 
+// Declare variables
+double letsGo,school,team,cheerOne,cheerTwo;
+double school = <<"FTCC"<< endl;
+double team = <<"Trojans"<< endl;
+double letsGo = <<"Lets's Go"
+
+cout cheerOne = letsGo + school;
+
+
+
+
+
+}
