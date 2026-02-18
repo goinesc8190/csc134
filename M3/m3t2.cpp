66e3fbc559eb98bdfb452a14a6ed722767a3d674
goinesc8190 <goinesc8190@student.faytechcc.edu>
2026-02-18 15:47:05 +0000

    m3t2 long version

--- M3/m3t2.cpp
+++ M3/m3t2.cpp
@@ -3,8 +3,9 @@
 // goinesc
 // 2/16/26
 // Start the game of craps.
-#include <iostream>
+
 using namespace std;
+#include <iostream>
 #include <cstdlib> // for random
 #include <ctime> //for time
 
@@ -20,12 +21,35 @@ int main() {
    
     int number = roll ();
     int first, second, total;
+   string result; // did we win or lose?
     first = roll ();
     second = roll ();
     total = first + second;
     cout << "You rolled a " << first << "+" << second << "=" << total << endl;
+    
+    // how did we do?
+    if (total == 7) {
+        result = "win";
+    }
+    else if (total == 11) {
+        result = "win";
+    }
+    else if (total == 2) {
+        result = "lose";
+    }
+    else if (total == 3) {
+        result = "lose";
+    }
+    else if (total == 12) {
+        result = "lose";
+    }
+    else {   
+    result = "lose";
+    } 
+    cout << " ROLL results " <<  result <<endl;
     return 0;
 }
+
 
 // Function definitions
 // (Actually write the Entire function!)
@@ -35,4 +59,6 @@ int roll() {
     // finally, add 1 so it'o to five.
     int my_roll = (rand() % 6) + 1;
     return my_roll; 
+
+
 }
