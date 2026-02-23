7cb13d11dc04458c5b7cb44019a5ad8abb9c0721
goinesc8190 <goinesc8190@student.faytechcc.edu>
2026-02-23 16:42:09 +0000

    Started Writing

---
+++ M3/m3lab1.cpp
@@ -0,0 +1,23 @@
+// m3lab1
+// goines
+// 2/23/2026
+
+#include <iostream>
+#include <iomanip>
+#include <cstlib>  
+using namespace std;
+
+void do_choice_1(1978 Pontiac Trans AM);
+void do_choice_2(1965_Lincoln_Continental);
+void do_choice_3();
+void do_choice_4();
+
+int main() {
+
+    int choice;
+    cout << "Elite Paint" << endl; 
+    cout << "Welcome to Elite Paint Demo! Make the following choices before entering the paint booth."<< endl; 
+    cout << "Are you driving a (1) 1978 Pontiac Trans Am or a (2) 1965 Lincoln Continental." << endl;
+    cout << 
+
+}
