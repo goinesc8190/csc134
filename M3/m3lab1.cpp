5d9f8030360e08a6a7c939e2b36248092b33b8b4
goinesc8190 <goinesc8190@student.faytechcc.edu>
2026-03-04 15:59:56 +0000

    completed

--- M3/m3lab1.cpp
+++ M3/m3lab1.cpp
@@ -13,20 +13,25 @@ void do_choice_3();
 void do_choice_4();
 
 int main() {
-
     int choice;
+    string do_choice_trans_am();
+    
     cout << "Elite Paint" << endl; 
     cout << "Welcome to Elite Paint Demo! Make the following choices before entering the paint booth."<< endl; 
-    cout << "Are you driving a (1) 1978 Pontiac Trans Am or a (2) 1965 Lincoln Continental." << endl;
+    cout << "Are you driving a or a"<< endl;
+    cout << "1. 1978 Pontiac Trans Am." << endl; 
+    cout << "2. 1965 Lincoln Continental." << endl;
+
+    cout << "Please choose choice one or choice two" << endl;
     cin >> choice;
+    
     if (choice == 1) {
-        do_choice_trans_am();
+        do_choice_trans_am() = "1978 Pontiac Trans AM";
     }
     else if (choice == 2) {
         do_choice_lincoln();
     }
 
-
-
+return 0; //end of game
 
 }
