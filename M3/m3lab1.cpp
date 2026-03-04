6d825ce9c30bf09e7307e8a6e67db79020b8c113
goinesc8190 <goinesc8190@student.faytechcc.edu>
2026-03-04 16:43:40 +0000

    Finished Choices For Trans AM

--- M3/m3lab1.cpp
+++ M3/m3lab1.cpp
@@ -9,16 +9,19 @@ using namespace std;
 
 void do_choice_trans_am(); // (1978 Pontiac Trans AM);
 void do_choice_lincoln();  // (1965_Lincoln_Continental);
-void do_choice_3();
-void do_choice_4();
+void do_choice_paint_red();
+void do_choice_paint_green();
+
+// Global variable -- keeps the vehicle choice
+string vehicle;
 
 int main() {
     int choice;
-    string do_choice_trans_am();
+    
     
     cout << "Elite Paint" << endl; 
     cout << "Welcome to Elite Paint Demo! Make the following choices before entering the paint booth."<< endl; 
-    cout << "Are you driving a or a"<< endl;
+    cout << "Are you driving"<< endl;
     cout << "1. 1978 Pontiac Trans Am." << endl; 
     cout << "2. 1965 Lincoln Continental." << endl;
 
@@ -26,12 +29,44 @@ int main() {
     cin >> choice;
     
     if (choice == 1) {
-        do_choice_trans_am() = "1978 Pontiac Trans AM";
+        do_choice_trans_am(); //  "1978 Pontiac Trans AM";
+        
     }
     else if (choice == 2) {
-        do_choice_lincoln();
+        do_choice_lincoln(); // "1965 Lincoln Continental";
+        // now call the paint choice
     }
 
 return 0; //end of game
 
 }
+
+void do_choice_trans_am() {
+    vehicle = "1978 Pontiac Trans AM";
+    cout << " Would you like to paint your car red or green? " << endl;
+    cout << " Please choose choice one or choice two" << endl;
+    cout << " 1. Red " << endl;
+    cout << " 2. Green " << endl;
+    
+    int choice;
+    cin >> choice; 
+    if (choice == 1) {
+    do_choice_paint_red(); 
+    }
+   else if (choice == 2)  { 
+    do_choice_paint_green();
+   }   
+}
+
+void do_choice_lincoln() {
+    vehicle = "1965 Lincoln Continental";
+}
+
+void do_choice_paint_red() {
+    cout << "You painted the " << vehicle << " red" << endl;
+
+}
+void do_choice_paint_green(){
+    cout << "You painted the " << vehicle << " green" << endl;
+}
+
