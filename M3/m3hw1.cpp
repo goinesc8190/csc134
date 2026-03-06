a610447d98d46e15e53cfaa597ad50409b09648a
goinesc8190 <goinesc8190@student.faytechcc.edu>
2026-03-06 17:56:04 +0000

    Parameters Set

---
+++ M3/m3hw1.cpp
@@ -0,0 +1,35 @@
+// Put comments here
+
+#include <iostream>
+using namespace std;
+
+// The questions
+void question1();
+void question2();
+void question3();
+void question4();
+
+// main
+int main() {
+    question1();
+    question2();
+    question3();
+    question4();
+}
+
+// The functions go here
+void question1() {
+
+}
+
+void question2() {
+
+}
+
+void question3() {
+    cout << "Q3: Done, see m3Lab1" << endl;
+}
+
+void question4() {
+
+}
