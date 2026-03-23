e1cd64e52d7a633e5e501a645f7abfd8c7891b24
goinesc8190 <goinesc8190@student.faytechcc.edu>
2026-03-23 22:15:45 +0000

    Bronze complete

---
+++ M4/m4hw1.cpp
@@ -0,0 +1,38 @@
+//M4HW1
+//CSC 134
+//goinesc
+//3/23/26
+
+
+#include <iostream>
+using namespace std;
+
+int main () {
+// declare variables 
+int firstNum, secondNum, answer;
+firstNum = 5;
+secondNum = 1;
+answer = firstNum * secondNum;
+
+cout << "Enter a number between one and 12"<< endl;
+cin >> 10 "times" 1 is 10 >>endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+for (int i=1; i<=12; i++) {
+    answer = firstNum * i;
+    cout << firstNum << " times " << i << " is " <<
+    answer << endl;
+} 
+
+}
