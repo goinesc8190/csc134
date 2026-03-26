96c549845c1a3dce4aded339f19e47846c47ab61
goinesc8190 <goinesc8190@student.faytechcc.edu>
2026-03-26 15:32:34 +0000

    Finished M4Lab1

--- M4/m4lab1.cpp
+++ M4/m4lab1.cpp
@@ -11,21 +11,13 @@ int width, height;
 width = 5;
 height = 5;
 
-for (int j=; j < width; j++) {
-    cout << "*" << " ";
-}
-cout << endl;
-
-for (int i=0; i < height; i++) {
-    cout << "*" << endl;
-}
-
 cout << "The entire block" << endl;
-for (int i=0; i < height; i++) {
-cout << "*" << endl;
-}
-for (int j=5; j < width; j++) {
-cout << endl;
 
+for (int i = 0; i < height; i++) {
+    cout << "*****"; 
+    for (int j = 0; j < width; j++) {
+         
+}
+cout << endl;
 }
 }
