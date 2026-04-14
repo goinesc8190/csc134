649b27ee7ea11facecb7e7d5c9c24884643b9e8a
goinesc8190 <goinesc8190@student.faytechcc.edu>
2026-04-14 17:38:11 +0000

    completed function definitions

--- M5/m5hw1.cpp
+++ M5/m5hw1.cpp
@@ -4,3 +4,47 @@
 //4/1/26
 
 #include <iostream>
+#include <string>
+using namespace std;
+
+// List all question prototypes here
+void question1();
+void question2();
+// rest go here
+
+
+// main() here
+int main() {
+
+    question1();
+    
+    question2();
+    // and so on
+    return 0;
+}
+
+// All full questions (function definitions) go here
+void question1() {
+    
+    string month1, month2, month3;
+    
+    cout << "Please enter the month of the rainfall" << endl;
+    cout << "Enter month one"<< endl;
+    cin >> month1;
+    cout << "Enter month two" << endl;
+    cin >> month2;
+    cout << "Enter month three" << endl;
+    cin >> month3; 
+
+    // Gather average rainfall for 3 months.
+}
+
+void question2() {
+   double rainfl1, rainfl2, rainfl3, average; 
+ cout << "Enter the amount of rainfall in inches for month one" << endl;
+    cin >> rainfl1; 
+ cout << "Enter the amount of rainfall in inches for month two" << endl;
+    cin >> rainfl2; 
+ cout << "Enter the amount of rainfall in inches for month three" << endl;
+    cin >> rainfl3; 
+}
